1f4b9c8e16f0bcba26bf819d9c683876f18d837d
Nikita Bakun <darty.vis@gmail.com>
2021-03-06 15:57:43 +0200

    bug with camera

--- videoandchat/videoPart/public/script.js
+++ videoandchat/videoPart/public/script.js
@@ -4,7 +4,8 @@ myVideo.muted = true
 let myVideoStream
 const videoGrid = document.getElementById('video-grid')
 
-var peer = new Peer(undefined, {
+// eslint-disable-next-line no-undef
+const peer = new Peer(undefined, {
 	path: '/peerjs',
 	host: '/',
 	port: '3000',
@@ -15,20 +16,34 @@ navigator.mediaDevices
 		audio: true,
 	})
 	.then((stream) => {
+		// eslint-disable-next-line no-unused-vars
 		myVideoStream = stream
 		addVideoStream(myVideo, stream)
+
+		peer.on('call', (call) => {
+			call.answer(stream)
+			const video = document.createElement('video')
+			call.on('stream', (userVideoStream) => {
+				addVideoStream(video, userVideoStream)
+			})
+		})
+
+		socket.on('user-connected', (userId) => {
+			connectToNewUser(userId, stream)
+		})
 	})
 
 peer.on('open', (id) => {
+	// eslint-disable-next-line no-undef
 	socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.on('user-connected', (userId) => {
-	connectToNewUser(userId)
-})
-
-const connectToNewUser = (userId) => {
-	console.log(userId)
+const connectToNewUser = (userId, stream) => {
+	const call = peer.call(userId, stream)
+	const video = document.createElement('video')
+	call.on('stream', (userVideoStream) => {
+		addVideoStream(video, userVideoStream)
+	})
 }
 
 const addVideoStream = (video, stream) => {
