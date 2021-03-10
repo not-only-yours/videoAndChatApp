ac2d5c1146f9dae07934612a6c53d3da3379fc62
Nikita Bakun <darty.vis@gmail.com>
2021-03-10 10:33:08 +0200

    added husky and runed it

--- chatandvideo/src/firebase.js
+++ chatandvideo/src/firebase.js
@@ -1,11 +1,11 @@
 /*eslint-disable */
 const firebaseConfig = {
-    apiKey: "AIzaSyD3OrFbwg35jgpDfrCUnSauNwWjvB8D29A",
-    authDomain: "chatpart-18f0f.firebaseapp.com",
-    projectId: "chatpart-18f0f",
-    storageBucket: "chatpart-18f0f.appspot.com",
-    messagingSenderId: "15268948799",
-    appId: "1:15268948799:web:97b729bdf7a62cf8b2818a",
-    measurementId: "G-PYZBV7J98Z"
-  };
+  apiKey: "AIzaSyD3OrFbwg35jgpDfrCUnSauNwWjvB8D29A",
+  authDomain: "chatpart-18f0f.firebaseapp.com",
+  projectId: "chatpart-18f0f",
+  storageBucket: "chatpart-18f0f.appspot.com",
+  messagingSenderId: "15268948799",
+  appId: "1:15268948799:web:97b729bdf7a62cf8b2818a",
+  measurementId: "G-PYZBV7J98Z",
+};
 /*eslint-enable */
