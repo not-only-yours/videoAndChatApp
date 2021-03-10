b6c001969cf64617992c62e6c817b245e2517d6d
Олександр Грибинюк <oleksandrgribinuk@MacBook-Pro-Oleksandr.local>
2021-03-10 13:57:45 +0200

    eslint . --fix

--- chatandvideo/src/App.js
+++ chatandvideo/src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import Leftpart from "./Leftpart";
+import './App.css'
+import Leftpart from './Leftpart'
 
 function App() {
-  return (
-    <div className="app">
-      <div className="app_body">
-        <Leftpart />
-        {/*Тут буде вікно самого чату */}
-      </div>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='app_body'>
+				<Leftpart />
+				{/*Тут буде вікно самого чату */}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- chatandvideo/src/Leftpart.js
+++ chatandvideo/src/Leftpart.js
@@ -1,41 +1,41 @@
-import React from "react";
-import "./Leftpart.css";
-import { IconButton } from "@material-ui/core";
-import AccessibleIcon from "@material-ui/icons/Accessible";
-import SearchIcon from "@material-ui/icons/Search";
-import InfoIcon from "@material-ui/icons/Info";
+import React from 'react'
+import './Leftpart.css'
+import { IconButton } from '@material-ui/core'
+import AccessibleIcon from '@material-ui/icons/Accessible'
+import SearchIcon from '@material-ui/icons/Search'
+import InfoIcon from '@material-ui/icons/Info'
 
 function Leftpart() {
-  return (
-    <div className="leftpart">
-      <div className="leftpart_header">
-        <AccessibleIcon />
-        <div className="leftpart_headerRight">
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-          <IconButton>
-            <InfoIcon />
-          </IconButton>
-        </div>
-      </div>
-      <div className="leftpart_search">
-        <div className="leftpart_searchContainer">
-          <SearchIcon />
-          <input type="text" placeholder="Print here dude" />
-        </div>
-      </div>
-      <div className="leftpart_chats">
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-        <h1>Chat</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className='leftpart'>
+			<div className='leftpart_header'>
+				<AccessibleIcon />
+				<div className='leftpart_headerRight'>
+					<IconButton>
+						<SearchIcon />
+					</IconButton>
+					<IconButton>
+						<InfoIcon />
+					</IconButton>
+				</div>
+			</div>
+			<div className='leftpart_search'>
+				<div className='leftpart_searchContainer'>
+					<SearchIcon />
+					<input type='text' placeholder='Print here dude' />
+				</div>
+			</div>
+			<div className='leftpart_chats'>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+				<h1>Chat</h1>
+			</div>
+		</div>
+	)
 }
 
-export default Leftpart;
+export default Leftpart
