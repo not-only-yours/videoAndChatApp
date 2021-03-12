bae090fc300e975f9f1d89f519607f91d15c98ec
Oleksandr Hrybyniuk <you@example.com>
2021-03-12 18:15:30 +0200

    Fixed error with condition/ added css

--- chatandvideo/src/LeftChats.js
+++ chatandvideo/src/LeftChats.js
@@ -3,21 +3,19 @@ import "./LeftChats.css";
 import { Avatar } from "@material-ui/core";
 
 function LeftChats({ addProp }) {
-  if (!addProp) {
-    return (
-      <div className="leftpart_chat">
-        <Avatar />
-        <div className="leftpartChat_info">
-          <h2>Назва чату</h2>
-          <p>Останнє повідомлення</p>
-        </div>
+  return !addProp ? (
+    <div className="leftpart_chat">
+      <Avatar />
+      <div className="leftpartChat_info">
+        <h2>Назва чату</h2>
+        <p>Останнє повідомлення</p>
       </div>
-    );
-  } else {
+    </div>
+  ) : (
     <div /*onClick={createChat} */ className="leftpart_chat">
       <h2>Добавити новий чатік</h2>
-    </div>;
-  }
+    </div>
+  );
 }
 
 export default LeftChats;
