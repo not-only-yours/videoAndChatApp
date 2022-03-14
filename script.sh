515bd67c5395e7d633dcd01c8de648a2bf3e938b
not-only-yours <darty.vis@gmail.com>
2022-03-14 18:03:34 +0200

    added -y to apt install

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 echo "get dependencies"
-apt update
-apt install docker.io certbot
+apt update -y
+apt install docker.io certbot -y
 curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 
 echo "generate certs"
-certbot certonly --standalone -d nikitasdomain.tk --staple-ocsp -m darty@nikitasdomain.tk --agree-tos
+certbot certonly --standalone -d slave.nikitasdomain.tk --staple-ocsp -m darty@nikitasdomain.tk --agree-tos
 
 
 echo "create network"
@@ -15,5 +15,3 @@ sudo docker network create my-pre-existing-network
 
 echo "run docker compose"
 docker-compose up -d
-
-CMD ["npm", "start"]
