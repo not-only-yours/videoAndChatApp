46824cd60e4cd1cc2218ab7e8ad8506669ad4577
not-only-yours <darty.vis@gmail.com>
2022-03-14 10:29:30 +0200

    added shell script

---
+++ chatandvideo/script.sh
@@ -0,0 +1,9 @@
+echo "get dependencies"
+apt update
+apt install docker.io
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+echo "run docker compose"
+docker-compose up -d
