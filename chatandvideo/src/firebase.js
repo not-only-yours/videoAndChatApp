a6cdf28b58ffe26a7e783ab206a27daa2d30957d
not-only-yours <darty.vis@gmail.com>
2022-03-22 15:52:14 +0200

    edited firestore

--- chatandvideo/src/firebase.js
+++ chatandvideo/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3OrFbwg35jgpDfrCUnSauNwWjvB8D29A",
-  authDomain: "chatpart-18f0f.firebaseapp.com",
-  databaseURL: "https://chatpart-18f0f-default-rtdb.firebaseio.com",
-  projectId: "chatpart-18f0f",
-  storageBucket: "chatpart-18f0f.appspot.com",
-  messagingSenderId: "15268948799",
-  appId: "1:15268948799:web:97b729bdf7a62cf8b2818a",
-  measurementId: "G-PYZBV7J98Z",
+  apiKey: "AIzaSyCybuFbMNItTvWFSmhFr5NOVLEIzb2HyZw",
+  authDomain: "videochatapp-3d7d5.firebaseapp.com",
+  projectId: "videochatapp-3d7d5",
+  storageBucket: "videochatapp-3d7d5.appspot.com",
+  messagingSenderId: "534185277677",
+  appId: "1:534185277677:web:8f646bdc25f269c422b4e4",
+  measurementId: "G-EBR83FKZWW",
+  databaseURL: "https://videochatapp-3d7d5.firebaseio.com",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
