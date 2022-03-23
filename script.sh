13d361b3c73a5272ed72c81ce4a71f79891fb9c5
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2022-03-23 15:49:59 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,8 @@
-echo "get dependencies"
-apt update -y
-apt install docker.io certbot -y
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-
-echo "generate certs"
-certbot certonly --standalone -d slave.nikitasdomain.tk --staple-ocsp -m darty@nikitasdomain.tk --agree-tos
-
-
-echo "create network"
-sudo docker network create my-pre-existing-network
-
+echo "change folder"
+cd /opt/application/workspace/VideoChat
 
 echo "run docker compose"
-docker-compose up -d
+docker-compose up -d --no-cache
+
+echo "rerun proxy"
+docker start nginx
