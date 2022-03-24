a167a15c4f2b8d00d9c9f657cfce159f7085f70f
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2022-03-24 11:53:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ echo "change folder"
 cd /opt/application/workspace/VideoChat
 
 echo "run docker compose"
-docker-compose up -d --no-cache
+docker-compose up -d --force-recreate
 
 echo "rerun proxy"
-docker start nginx
+docker exec nginx nginx -s reload
