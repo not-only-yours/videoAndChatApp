65575252283bed8cbd0dd70403353fb609f1ca00
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2022-04-07 15:02:34 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,32 @@
-echo "change folder"
-cd /opt/application/workspace/VideoChat
+#!/bin/bash
 
-echo "run docker compose"
-docker-compose up -d --force-recreate
+function containers () {
+ docker ps --format "{{.ID}}:{{.Image}}" | sed 's/:[^:]*//' | sed 's/\..*//'
+}
 
-echo "rerun proxy"
-docker exec nginx nginx -s reload
+function branches () {
+ git branch -a | grep 'remotes/origin/features/' | rev | sed 's/\/.*//' | rev
+}
+
+function getContainerId () {
+ echo "$1" | sed 's/\:.*//'
+}
+
+for CONTAINER in $(containers); do
+        isBranch=false
+        for BRANCH in $(branches); do
+                if [[ "$CONTAINER" = *"$BRANCH"* ]]; then
+                #echo $CONTAINER
+                #echo $BRANCH
+                $isBranch=true
+                fi
+        done
+
+        if [ $isBranch == false ]; then
+                echo $(getContainerId $CONTAINER)
+                docker stop $(getContainerId $CONTAINER)
+                docker rm $(getContainerId $CONTAINER)
+        fi
+done
+
+docker image prune -a -f
