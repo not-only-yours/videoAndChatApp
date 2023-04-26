12739dc1c270eaac996e48cdbee7f0b05666370a
not-only-yours <darty.vis@gmail.com>
2023-04-26 14:45:10 +0300

    fixed env vars

--- chatandvideo/src/firebase.js
+++ chatandvideo/src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase';
 
-const env = {
-  apiKey: 'AIzaSyCybuFbMNItTvWFSmhFr5NOVLEIzb2HyZw',
-  authDomain: 'videochatapp-3d7d5.firebaseapp.com',
-  projectId: 'videochatapp-3d7d5',
-  storageBucket: 'videochatapp-3d7d5.appspot.com',
-  messagingSenderId: '534185277677',
-  appId: '1:534185277677:web:8f646bdc25f269c422b4e4;measurementId=G-EBR83FKZWW',
-  databaseURL: 'https://videochatapp-3d7d5.firebaseio.com',
-};
+const env = JSON.parse(process.env.REACT_APP_VideoChat);
 
 const firebaseConfig = {
   apiKey: env.apiKey,
